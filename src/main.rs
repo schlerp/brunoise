6cd664290f41de3123d1c1b11636e0650aa10fa3
PattyC <2208036+schlerp@users.noreply.github.com>
2023-10-08 06:36:33 +0000

    feat: added scopes and restructed into modules (#7)

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,36 @@
-include!("../pyembedded/default_python_config.rs");
+use actix_web::{web, App, HttpServer};
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-async fn python_hello_world() -> impl Responder {
-    println!("Running python hello world...");
-    // Get config from default_python_config.rs.
-    let config = default_python_config();
-
-    println!("Setting up pyembed interp...");
-    let interp = pyembed::MainPythonInterpreter::new(config).unwrap();
-
-    println!("Running interp with GIL...");
-    // `py` is a `pyo3::Python` instance.
-    interp.with_gil(|py| {
-        py.run("print('from python: hello, world')", None, None)
-            .unwrap();
-    });
-
-    println!("Successfully ran Python!");
-    HttpResponse::Ok().body("I ran python!")
-}
+mod endpoints;
+use crate::endpoints::python_endpoints;
+use crate::endpoints::test_endpoints;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-            .route("/py", web::get().to(python_hello_world))
+            .service(
+                web::scope("config")
+                    .app_data(web::Data::new(test_endpoints::HelloFromState {
+                        greeter_name: String::from("config"),
+                    }))
+                    .route("/hey", web::get().to(test_endpoints::hello_from)),
+            )
+            .service(
+                web::scope("events")
+                    .app_data(web::Data::new(test_endpoints::HelloFromState {
+                        greeter_name: String::from("events"),
+                    }))
+                    .route("/hey", web::get().to(test_endpoints::hello_from)),
+            )
+            .service(
+                web::scope("test")
+                    .service(test_endpoints::echo)
+                    .route("/py", web::get().to(python_endpoints::python_hello_world))
+                    .app_data(web::Data::new(test_endpoints::HelloFromState {
+                        greeter_name: String::from("events"),
+                    }))
+                    .route("/hey", web::get().to(test_endpoints::hello_from)),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
